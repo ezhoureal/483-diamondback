f73b074aac221264d8c0df6d5a774a00c684c8fc
Tianer Zhou <zhoutianer@huawei.com>
2024-08-01 20:42:22 +0800

    complete uniquify
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>

--- src/compile.rs
+++ src/compile.rs
@@ -730,6 +730,10 @@ where
     Span: Clone,
 {
     check_prog(p)?;
+    let unique_p = uniquify(&p, &mut HashMap::new(), &mut 0);
+    let (decls, main) = lambda_lift(&unique_p);
+    let program = seq_prog(&decls, &main);
+
     let seq = sequentialize(p, &mut 0);
     let max_stack = space_needed(&seq);
     let main_is = compile_to_instrs(&seq, max_stack);
@@ -752,8 +756,96 @@ start_here:
     Ok(res)
 }
 
-fn uniquify(e: &Exp<u32>) -> Exp<()> {
-    panic!("NYI: uniquify")
+fn uniquify<Span>(e: &Exp<Span>, mapping: &HashMap<String, String>, counter: &mut u32) -> Exp<()> {
+    match e {
+        Exp::Let {
+            bindings,
+            body,
+            ann,
+        } => {
+            let mut scoped_mapping = mapping.clone();
+            let mut_bind = bindings
+                .iter()
+                .map(|(var, value)| {
+                    *counter += 1;
+                    let new_var = format!("{}", counter);
+                    scoped_mapping.insert(new_var.clone(), var.to_string());
+                    return (new_var, uniquify(value, mapping, counter));
+                })
+                .collect();
+            Exp::Let {
+                bindings: mut_bind,
+                body: Box::new(uniquify(&body, &scoped_mapping, counter)),
+                ann: (),
+            }
+        }
+        Exp::FunDefs { decls, body, ann } => {
+            let mut scoped_mapping = mapping.clone();
+            for decl in decls {
+                *counter += 1;
+                let new_name = format!("{}", counter);
+                scoped_mapping.insert(new_name.clone(), decl.name.to_string());
+            }
+            let uniq_decls = decls
+                .iter()
+                .map(|decl| FunDecl {
+                    name: scoped_mapping[&decl.name].clone(),
+                    parameters: decl
+                        .parameters
+                        .iter()
+                        .map(|param| scoped_mapping[param].clone())
+                        .collect(),
+                    body: uniquify(&body, &scoped_mapping, counter),
+                    ann: (),
+                })
+                .collect();
+            Exp::FunDefs {
+                decls: uniq_decls,
+                body: Box::new(uniquify(&body, mapping, counter)),
+                ann: (),
+            }
+        }
+        Exp::Var(v, _) => Exp::Var(mapping[v].clone(), ()),
+        Exp::Num(i, _) => Exp::Num(*i, ()),
+        Exp::Bool(b, _) => Exp::Bool(*b, ()),
+        Exp::Prim(op, subjects, _) => {
+            let uniq_sub = subjects
+                .iter()
+                .map(|s| Box::new(uniquify(s, mapping, counter)))
+                .collect();
+            Exp::Prim(*op, uniq_sub, ())
+        }
+        Exp::If {
+            cond,
+            thn,
+            els,
+            ann: _,
+        } => Exp::If {
+            cond: Box::new(uniquify(&cond, mapping, counter)),
+            thn: Box::new(uniquify(&thn, mapping, counter)),
+            els: Box::new(uniquify(&els, mapping, counter)),
+            ann: (),
+        },
+        Exp::Call(func, params, _) => {
+            let uniq_params = params
+                .iter()
+                .map(|s| uniquify(s, mapping, counter))
+                .collect();
+            Exp::Call(mapping[func].clone(), uniq_params, ())
+        }
+        Exp::InternalTailCall(_, _, _) => Exp::InternalTailCall(String::new(), vec![], ()),
+        Exp::ExternalCall {
+            fun_name: _,
+            args: _,
+            is_tail,
+            ann: _,
+        } => Exp::ExternalCall {
+            fun_name: String::new(),
+            args: vec![],
+            is_tail: *is_tail,
+            ann: (),
+        },
+    }
 }
 
 // Identify which functions should be lifted to the top level
@@ -766,6 +858,6 @@ fn lambda_lift<Ann>(p: &Exp<Ann>) -> (Vec<FunDecl<Exp<()>, ()>>, Exp<()>) {
     panic!("NYI: lambda_lift")
 }
 
-fn seq_prog(decls: &[FunDecl<Exp<u32>, u32>], p: &Exp<u32>) -> SeqProg<()> {
+fn seq_prog(decls: &[FunDecl<Exp<()>, ()>], p: &Exp<()>) -> SeqProg<()> {
     panic!("NYI: seq_prog")
 }
